3f07df81f3daf2bffff8d34b3ddca6f2009f29c5
Gustavo Gilberto Godinho <gustavo.godinho@ituran.com.br>
2019-09-27 10:57:14 -0300

    Add swagger dapper

--- aspnetcore-estudo-api-swagger-dapper/Startup.cs
+++ aspnetcore-estudo-api-swagger-dapper/Startup.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
-namespace aspnetcore_estudo_api_swagger_dapper
+namespace APIIndicadores
 {
     public class Startup
     {
@@ -22,13 +16,29 @@ namespace aspnetcore_estudo_api_swagger_dapper
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c => {
+
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "Minha primeira Api Swagger",
+                        Version = "v1",
+                        Description = "Estudo de API REST criada com o ASP.NET Core 2.2 para consulta a indicadores econômicos",
+                        Contact = new Contact
+                        {
+                            Name = "Gustavo Godinho",
+                            Email = "gustavogodinho6@msn.com",
+                            Url = "https://github.com/gustavogodinho"
+
+                        }
+                    });
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -37,9 +47,15 @@ namespace aspnetcore_estudo_api_swagger_dapper
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // middlewares para uso do Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Indicadores Econômicos V1");
+            });
+
 
             app.UseHttpsRedirection();
             app.UseMvc();
